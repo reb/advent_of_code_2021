a6d1cc818a7efcdf3a84660e29a232247bea79b9
reb <ruben.boumans@urbint.com>
2021-12-04 16:55:10 -0500

    Figure out which board wins first

--- src/day_04.rs
+++ src/day_04.rs
@@ -80,9 +80,20 @@ use std::collections::HashMap;
 const INPUT: &str = include_str!("../input/day_04");
 
 pub fn run() {
-    let (numbers, boards) = load_bingo_game(INPUT);
-    println!("numbers: {:?}", numbers);
-    println!("boards: {:?}", boards);
+    let (numbers, mut boards) = load_bingo_game(INPUT);
+
+    'game: for number in numbers.iter() {
+        for board in boards.iter_mut() {
+            board.mark(number);
+            if board.bingo() {
+                println!(
+                    "Found a board that will have a final score of: {}",
+                    board.score() * number
+                );
+                break 'game;
+            }
+        }
+    }
 }
 
 type BingoNumber = u32;
@@ -100,6 +111,52 @@ impl BingoBoard {
             numbers,
             marked: HashMap::new(),
         }
+    }
+
+    fn mark(&mut self, number: &BingoNumber) {
+        for (position, _) in self.numbers.iter().filter(|(_, n)| n == &number) {
+            self.marked.insert(*position, true);
+        }
+    }
+
+    fn bingo(&self) -> bool {
+        // check rows
+        let mut row = 0;
+        while self.numbers.get(&(row, 0)).is_some() {
+            if self
+                .numbers
+                .keys()
+                .filter(|(x, _)| x == &row)
+                .all(|pos| *self.marked.get(pos).unwrap_or(&false))
+            {
+                return true;
+            }
+            row += 1;
+        }
+
+        // check columns
+        let mut column = 0;
+        while self.numbers.get(&(0, column)).is_some() {
+            if self
+                .numbers
+                .keys()
+                .filter(|(_, y)| y == &column)
+                .all(|pos| *self.marked.get(pos).unwrap_or(&false))
+            {
+                return true;
+            }
+            column += 1;
+        }
+
+        return false;
+    }
+
+    fn score(&self) -> BingoNumber {
+        self.numbers
+            .iter()
+            .filter(|(pos, _)| !*self.marked.get(pos).unwrap_or(&false))
+            .map(|(_, number)| number)
+            .sum()
     }
 }
 
@@ -137,6 +194,126 @@ fn parse_board(board: &str) -> BingoBoard {
 mod tests {
     use super::*;
 
+    fn bingo_board_1() -> BingoBoard {
+        // 22 13 17 11  0
+        //  8  2 23  4 24
+        // 21  9 14 16  7
+        //  6 10  3 18  5
+        //  1 12 20 15 19
+        BingoBoard::new(
+            [
+                ((0, 0), 22),
+                ((0, 1), 13),
+                ((0, 2), 17),
+                ((0, 3), 11),
+                ((0, 4), 0),
+                ((1, 0), 8),
+                ((1, 1), 2),
+                ((1, 2), 23),
+                ((1, 3), 4),
+                ((1, 4), 24),
+                ((2, 0), 21),
+                ((2, 1), 9),
+                ((2, 2), 14),
+                ((2, 3), 16),
+                ((2, 4), 7),
+                ((3, 0), 6),
+                ((3, 1), 10),
+                ((3, 2), 3),
+                ((3, 3), 18),
+                ((3, 4), 5),
+                ((4, 0), 1),
+                ((4, 1), 12),
+                ((4, 2), 20),
+                ((4, 3), 15),
+                ((4, 4), 19),
+            ]
+            .iter()
+            .cloned()
+            .collect(),
+        )
+    }
+
+    fn bingo_board_2() -> BingoBoard {
+        //  3 15  0  2 22
+        //  9 18 13 17  5
+        // 19  8  7 25 23
+        // 20 11 10 24  4
+        // 14 21 16 12  6
+        BingoBoard::new(
+            [
+                ((0, 0), 3),
+                ((0, 1), 15),
+                ((0, 2), 0),
+                ((0, 3), 2),
+                ((0, 4), 22),
+                ((1, 0), 9),
+                ((1, 1), 18),
+                ((1, 2), 13),
+                ((1, 3), 17),
+                ((1, 4), 5),
+                ((2, 0), 19),
+                ((2, 1), 8),
+                ((2, 2), 7),
+                ((2, 3), 25),
+                ((2, 4), 23),
+                ((3, 0), 20),
+                ((3, 1), 11),
+                ((3, 2), 10),
+                ((3, 3), 24),
+                ((3, 4), 4),
+                ((4, 0), 14),
+                ((4, 1), 21),
+                ((4, 2), 16),
+                ((4, 3), 12),
+                ((4, 4), 6),
+            ]
+            .iter()
+            .cloned()
+            .collect(),
+        )
+    }
+
+    fn bingo_board_3() -> BingoBoard {
+        // 14 21 17 24  4
+        // 10 16 15  9 19
+        // 18  8 23 26 20
+        // 22 11 13  6  5
+        //  2  0 12  3  7
+        BingoBoard::new(
+            [
+                ((0, 0), 14),
+                ((0, 1), 21),
+                ((0, 2), 17),
+                ((0, 3), 24),
+                ((0, 4), 4),
+                ((1, 0), 10),
+                ((1, 1), 16),
+                ((1, 2), 15),
+                ((1, 3), 9),
+                ((1, 4), 19),
+                ((2, 0), 18),
+                ((2, 1), 8),
+                ((2, 2), 23),
+                ((2, 3), 26),
+                ((2, 4), 20),
+                ((3, 0), 22),
+                ((3, 1), 11),
+                ((3, 2), 13),
+                ((3, 3), 6),
+                ((3, 4), 5),
+                ((4, 0), 2),
+                ((4, 1), 0),
+                ((4, 2), 12),
+                ((4, 3), 3),
+                ((4, 4), 7),
+            ]
+            .iter()
+            .cloned()
+            .collect(),
+        )
+    }
+
     #[test]
     fn test_load_bingo_game() {
         let input = "\
@@ -152,79 +329,72 @@ mod tests {
          9 18 13 17  5\n\
         19  8  7 25 23\n\
         20 11 10 24  4\n\
-        14 21 16 12  6\n";
+        14 21 16 12  6\n\
+        \n\
+        14 21 17 24  4\n\
+        10 16 15  9 19\n\
+        18  8 23 26 20\n\
+        22 11 13  6  5\n\
+         2  0 12  3  7\n";
 
         let expected_numbers = vec![
             7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19,
             3, 26, 1,
         ];
-        let expected_boards = vec![
-            BingoBoard::new(
-                [
-                    ((0, 0), 22),
-                    ((0, 1), 13),
-                    ((0, 2), 17),
-                    ((0, 3), 11),
-                    ((0, 4), 0),
-                    ((1, 0), 8),
-                    ((1, 1), 2),
-                    ((1, 2), 23),
-                    ((1, 3), 4),
-                    ((1, 4), 24),
-                    ((2, 0), 21),
-                    ((2, 1), 9),
-                    ((2, 2), 14),
-                    ((2, 3), 16),
-                    ((2, 4), 7),
-                    ((3, 0), 6),
-                    ((3, 1), 10),
-                    ((3, 2), 3),
-                    ((3, 3), 18),
-                    ((3, 4), 5),
-                    ((4, 0), 1),
-                    ((4, 1), 12),
-                    ((4, 2), 20),
-                    ((4, 3), 15),
-                    ((4, 4), 19),
-                ]
-                .iter()
-                .cloned()
-                .collect(),
-            ),
-            BingoBoard::new(
-                [
-                    ((0, 0), 3),
-                    ((0, 1), 15),
-                    ((0, 2), 0),
-                    ((0, 3), 2),
-                    ((0, 4), 22),
-                    ((1, 0), 9),
-                    ((1, 1), 18),
-                    ((1, 2), 13),
-                    ((1, 3), 17),
-                    ((1, 4), 5),
-                    ((2, 0), 19),
-                    ((2, 1), 8),
-                    ((2, 2), 7),
-                    ((2, 3), 25),
-                    ((2, 4), 23),
-                    ((3, 0), 20),
-                    ((3, 1), 11),
-                    ((3, 2), 10),
-                    ((3, 3), 24),
-                    ((3, 4), 4),
-                    ((4, 0), 14),
-                    ((4, 1), 21),
-                    ((4, 2), 16),
-                    ((4, 3), 12),
-                    ((4, 4), 6),
-                ]
-                .iter()
-                .cloned()
-                .collect(),
-            ),
-        ];
+        let expected_boards = vec![bingo_board_1(), bingo_board_2(), bingo_board_3()];
 
         assert_eq!(load_bingo_game(input), (expected_numbers, expected_boards));
+    }
+
+    #[test]
+    fn test_bingo_board_mark() {
+        let mut bingo_board = bingo_board_1();
+
+        let mut expected_marked = HashMap::new();
+        expected_marked.insert((1, 2), true);
+
+        bingo_board.mark(&23);
+
+        assert_eq!(bingo_board.marked, expected_marked);
+    }
+
+    #[test]
+    fn test_bingo_board_bingo() {
+        let mut bingo_board_1 = bingo_board_1();
+        let mut bingo_board_2 = bingo_board_2();
+        let mut bingo_board_3 = bingo_board_3();
+
+        for n in [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21].iter() {
+            // none of these should be bingo
+            bingo_board_1.mark(n);
+            assert!(!bingo_board_1.bingo());
+            bingo_board_2.mark(n);
+            assert!(!bingo_board_2.bingo());
+            bingo_board_3.mark(n);
+            assert!(!bingo_board_3.bingo());
+        }
+
+        let n = &24;
+        bingo_board_1.mark(n);
+        assert!(!bingo_board_1.bingo());
+        bingo_board_2.mark(n);
+        assert!(!bingo_board_2.bingo());
+        // finally bingo
+        bingo_board_3.mark(n);
+        println!("{:?}", bingo_board_3.marked);
+        println!("{:?}", bingo_board_3.numbers);
+        assert!(bingo_board_3.bingo());
+    }
+
+    #[test]
+    fn test_bingo_board_score() {
+        let mut bingo_board = bingo_board_3();
+
+        // prepare the board
+        for n in [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24].iter() {
+            bingo_board.mark(n);
+        }
+
+        assert_eq!(bingo_board.score(), 188);
     }
 }
